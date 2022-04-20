32b091d9873229ca8fcd21a4da84d0d54bbff12d
Tony Pernicano <pernicat@gmail.com>
2022-04-20 11:01:31 -0400

    cleans up configuration files

--- config.py
+++ config.py
@@ -11,15 +11,6 @@ class Config:  # pylint: disable=too-few-public-methods
     """configuration"""
     ROOT_DIR = Path(__file__).parent
 
-    HOST = "services3.arcgis.com"
-    CONTEXT = "Jdnp1TjADvSDxMAX/ArcGIS"
-    SERVICE_NAME = "DNR_ROADS"
-    SERVICE_TYPE = "FeatureServer"
-    SERVICE_LAYER = 0
-    OPERATION = "query"
-    SERVICE_URL = f"https://{HOST}/{CONTEXT}/rest/services/{SERVICE_NAME}/{SERVICE_TYPE}"
-    QUERY_URL = f"{SERVICE_URL}/{SERVICE_LAYER}/{OPERATION}"
-
     MI_DNR_ROADS_URL = (
         "https://services3.arcgis.com/Jdnp1TjADvSDxMAX/ArcGIS"
         "/rest/services/DNR_ROADS/FeatureServer"
@@ -30,24 +21,7 @@ class Config:  # pylint: disable=too-few-public-methods
     )
 
     MAPBOX_USER = "pernicat"
-    MAPBOX_ID = "MI_DNR_ROADS_2022"
+
     # https://account.mapbox.com/access-tokens
     # scopes: TILESET:LIST, TILESET:READ, TILESET:WRITE, DATASETS:READ, DATASETS:LIST
     MAPBOX_ACCESS_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
-
-    RECIPE_MI_DNR_ROADS = ROOT_DIR / "recipes" / "mi_dnr_roads_2022.json"
-    ATTRIBUTION = [
-        {
-            "text": "Michigan DNR 2022",
-            "link": (
-                "https://services3.arcgis.com/Jdnp1TjADvSDxMAX/ArcGIS"
-                "/rest/services/DNR_ROADS/FeatureServer"
-            ),
-        }
-    ]
-
-    DESCRIPTION = "Michigan DNR roads 2022"
-
-    MAPBOX_TILESET_NAME = f"{MAPBOX_USER}.{MAPBOX_ID}"
-
-    OUTPUT_FILE = "arcgis.jsonl"
